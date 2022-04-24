893dd4abf058c2a2d54bfa0dd6d156a430bfaa1b
1337vt <32816722+1337vt@users.noreply.github.com>
2022-04-24 09:30:21 -0500

    Add files via upload

---
+++ PowerShellBrowser.ps1
@@ -0,0 +1,34 @@
+﻿[xml]$Global:xmlWPF = '<Window x:Class="System.Windows.Window"
+        xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+        xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+        xmlns:d="http://schemas.microsoft.com/expression/blend/2008"
+        xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006"
+        Title="MainWindow" Height="450" Width="800">
+    <Grid>
+        <Button x:Name="backButton" Content="Back" HorizontalAlignment="Left" Margin="10,10,0,0" VerticalAlignment="Top" Width="50"/>
+        <Button x:Name="fwdButton" Content="Forward" HorizontalAlignment="Left" Margin="65,10,0,0" VerticalAlignment="Top" Width="50"/>
+        <TextBox x:Name="addressBar" Margin="175,10,0,0" TextWrapping="Wrap" Text="http://www.google.com" VerticalAlignment="Top"/>
+        <Button x:Name="goButton" Content="Go!" HorizontalAlignment="Left" Margin="120,10,0,0" VerticalAlignment="Top" Width="50"/>
+        <WebBrowser x:Name="webBrowserForm" Margin="0,35,0,0"/>
+
+    </Grid>
+</Window>'
+
+try{ Add-Type -AssemblyName PresentationCore,PresentationFramework,WindowsBase,system.windows.forms
+} catch {
+Throw "Failed to load WPF."}
+
+$Global:xamGUI = [Windows.Markup.XamlReader]::Load((New-Object System.Xml.XmlNodeReader $xmlWPF))
+$xmlWPF.SelectNodes("//*[@*[contains(translate(name(.),'n','N'),'Name')]]") | %{Set-Variable -Name ($_.Name) -Value $xamGUI.FindName($_.Name) -Scope Global}
+
+$goButton = $xamGUI.FindName("goButton")
+$fwdButton = $xamGUI.FindName("fwdButton")
+$backButton = $xamGUI.FindName("backButton")
+$addressBar = $xamGUI.FindName("addressBar")
+$webBrowserForm = $xamGUI.FindName("webBrowserForm")
+
+$goButton.add_click({$webBrowserForm.Navigate($addressBar.Text)})
+$fwdButton.add_click({$webBrowserForm.GoForward()})
+$backButton.add_click({$webBrowserForm.GoBack()})
+
+$xamGUI.ShowDialog() | Out-Null
